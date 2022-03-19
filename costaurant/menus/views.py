b80c669e71dbcd3ced6ae37d10374fd2e5c36cb0
toyo30 <toyo30@naver.com>
2022-03-19 23:36:52 +0900

    add: menu/urls/view

--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def index(request):
-    # return HttpResponse("<h2>Hellow, Django!</h2>")
-    return render(request, 'foods/index.html')
+    return HttpResponse("<h2>코스토랑 오픈!</h2>")
+    # return render(request, 'foods/index.html')
