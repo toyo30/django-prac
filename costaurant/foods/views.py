1b288f4ecf848abe795006235820de066a0069ee
toyo30 <toyo30@naver.com>
2022-03-20 00:53:48 +0900

    add: study/request/date

--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     # return HttpResponse("<h2>Hellow, Django!</h2>")
-    return render(request, 'foods/index.html')
+    today = datetime.today().date()
+    #render 함수. 첫번째 변수 호출 방법, 2번째 변수 호출 값 3번째 변수 데이터
+    context = {"date":today}
+    return render(request, 'foods/index.html', context=context)
