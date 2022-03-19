d1b67fe0d741773fa3d28e4fc4fe6f3e23316443
toyo30 <toyo30@naver.com>
2022-03-20 00:17:48 +0900

    add: menu/templage/views

--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h2>코스토랑 오픈!</h2>")
+    # return HttpResponse("<h2>코스토랑 오픈!</h2>")
+    return render(request, 'menus/index.html')
     # return render(request, 'foods/index.html')
